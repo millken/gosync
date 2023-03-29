b885bafa93e1982cc316fc3ea0d9f544d76f5bb0
millken <millken@gmail.com>
2023-03-29 18:56:43 +0800

    sync.Map generic version

--- map.go
+++ map.go
@@ -1,42 +1,299 @@
+// inspired by https://github.com/a8m/syncmap, but it is not generic and requires auto-generation, so I rewrote it.
 package gosync
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+	"unsafe"
+)
 
-type Map[K comparable, V any] struct {
-	m sync.Map
+type entry[V any] struct {
+	p        atomic.Pointer[V]
+	expunged unsafe.Pointer
 }
 
-// Delete deletes the value for a key.
-func (m *Map[K, V]) Delete(key K) { m.m.Delete(key) }
+func newEntry[V any](i V) *entry[V] {
+	e := &entry[V]{
+		expunged: unsafe.Pointer(new(V)),
+	}
+	e.p.Store(&i)
+	return e
+}
+
+func (e *entry[V]) load() (value V, ok bool) {
+	p := e.p.Load()
+	if p == nil || p == (*V)(e.expunged) {
+		return value, false
+	}
+	return *p, true
+}
+
+func (e *entry[V]) tryStore(v V) bool {
+	for {
+		p := e.p.Load()
+		if p == (*V)(e.expunged) {
+			return false
+		}
+		if e.p.CompareAndSwap(p, &v) {
+			return true
+		}
+	}
+}
+
+func (e *entry[V]) unexpungeLocked() (wasExpunged bool) {
+	return e.p.Swap((*V)(e.expunged)) == (*V)(e.expunged)
+}
+
+func (e *entry[V]) storeLocked(v V) {
+	e.p.Store(&v)
+}
+
+// tryLoadOrStore atomically loads or stores a value if the entry is not
+// expunged.
+//
+// If the entry is expunged, tryLoadOrStore leaves the entry unchanged and
+// returns with ok==false.
+func (e *entry[V]) tryLoadOrStore(v V) (actual V, loaded, ok bool) {
+	p := e.p.Load()
+	if p == (*V)(e.expunged) {
+		return actual, false, false
+	}
+	if p != nil {
+		return *p, true, true
+	}
+
+	// Copy the interface after the first load to make this method more amenable
+	// to escape analysis: if we hit the "load" path or the entry is expunged, we
+	// shouldn't bother heap-allocating.
+	ic := v
+	for {
+		if e.p.CompareAndSwap(nil, &ic) {
+			return v, false, true
+		}
+		p = e.p.Load()
+		if p == (*V)(e.expunged) {
+			return actual, false, false
+		}
+		if p != nil {
+			return *p, true, true
+		}
+	}
+}
+
+func (e *entry[V]) delete() (value V, ok bool) {
+	for {
+		p := e.p.Load()
+		if p == nil || p == (*V)(e.expunged) {
+			return value, false
+		}
+		if e.p.CompareAndSwap(p, nil) {
+			return *p, true
+		}
+	}
+}
+
+func (e *entry[V]) tryExpungeLocked() (isExpunged bool) {
+	p := e.p.Load()
+	for p == nil {
+		if e.p.CompareAndSwap(nil, (*V)(e.expunged)) {
+			return true
+		}
+		p = e.p.Load()
+	}
+	return p == (*V)(e.expunged)
+}
+
+// readOnly is an immutable struct stored atomically in the Map.read field.
+type readOnly[K comparable, V any] struct {
+	m       map[K]*entry[V]
+	amended bool // true if the dirty map contains some key not in m.
+}
+
+type Map[K comparable, V any] struct {
+	mu     sync.Mutex
+	read   atomic.Value
+	dirty  map[K]*entry[V]
+	misses int
+}
 
 // Load loads the value stored in the map for a key, or nil if no value is present.
 func (m *Map[K, V]) Load(key K) (value V, ok bool) {
-	v, ok := m.m.Load(key)
+	read, _ := m.read.Load().(readOnly[K, V])
+	e, ok := read.m[key]
+	if !ok && read.amended {
+		m.mu.Lock()
+		// Avoid reporting a spurious miss if m.dirty got promoted while we were
+		// blocked on m.mu. (If further loads of the same key will not miss, it's
+		// not worth copying the dirty map for this key.)
+		read, _ = m.read.Load().(readOnly[K, V])
+		e, ok = read.m[key]
+		if !ok && read.amended {
+			e, ok = m.dirty[key]
+			// Regardless of whether the entry was present, record a miss: this key
+			// will take the slow path until the dirty map is promoted to the read
+			// map.
+			m.missLocked()
+		}
+		m.mu.Unlock()
+	}
 	if !ok {
-		return value, ok
+		return value, false
 	}
-	return v.(V), ok
+	return e.load()
 }
 
-// LoadOrStore loads the value stored in the map for a key, or stores and returns the given value if no value is present.
-func (m *Map[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
-	v, loaded := m.m.LoadAndDelete(key)
-	if !loaded {
-		return value, loaded
+func (m *Map[K, V]) missLocked() {
+	m.misses++
+	if m.misses < len(m.dirty) {
+		return
 	}
-	return v.(V), loaded
+	m.read.Store(readOnly[K, V]{m: m.dirty})
+	m.dirty = nil
+	m.misses = 0
 }
 
-// LoadOrStore loads the value stored in the map for a key, or stores and returns the given value if no value is present.
+// Store sets the value for a key.
+func (m *Map[K, V]) Store(key K, value V) {
+	read, _ := m.read.Load().(readOnly[K, V])
+	if e, ok := read.m[key]; ok && e.tryStore(value) {
+		return
+	}
+
+	m.mu.Lock()
+	read, _ = m.read.Load().(readOnly[K, V])
+	if e, ok := read.m[key]; ok {
+		if e.unexpungeLocked() {
+			// The entry was previously expunged, which implies that there is a
+			// non-nil dirty map and this entry is not in it.
+			m.dirty[key] = e
+		}
+		e.storeLocked(value)
+	} else if e, ok := m.dirty[key]; ok {
+		e.storeLocked(value)
+	} else {
+		if !read.amended {
+			// We're adding the first new key to the dirty map.
+			// Make sure it is allocated and mark the read-only map as incomplete.
+			m.dirtyLocked()
+			m.read.Store(readOnly[K, V]{m: read.m, amended: true})
+		}
+		m.dirty[key] = newEntry(value)
+	}
+	m.mu.Unlock()
+}
+
+// LoadOrStore returns the existing value for the key if present.
+// Otherwise, it stores and returns the given value.
+// The loaded result is true if the value was loaded, false if stored.
 func (m *Map[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
-	a, loaded := m.m.LoadOrStore(key, value)
-	return a.(V), loaded
+	// Avoid locking if it's a clean hit.
+	read, _ := m.read.Load().(readOnly[K, V])
+	if e, ok := read.m[key]; ok {
+		actual, loaded, ok := e.tryLoadOrStore(value)
+		if ok {
+			return actual, loaded
+		}
+	}
+
+	m.mu.Lock()
+	read, _ = m.read.Load().(readOnly[K, V])
+	if e, ok := read.m[key]; ok {
+		if e.unexpungeLocked() {
+			m.dirty[key] = e
+		}
+		actual, loaded, _ = e.tryLoadOrStore(value)
+	} else if e, ok := m.dirty[key]; ok {
+		actual, loaded, _ = e.tryLoadOrStore(value)
+		m.missLocked()
+	} else {
+		if !read.amended {
+			// We're adding the first new key to the dirty map.
+			// Make sure it is allocated and mark the read-only map as incomplete.
+			m.dirtyLocked()
+			m.read.Store(readOnly[K, V]{m: read.m, amended: true})
+		}
+		m.dirty[key] = newEntry(value)
+		actual, loaded = value, false
+	}
+	m.mu.Unlock()
+
+	return actual, loaded
+}
+
+func (m *Map[K, V]) dirtyLocked() {
+	if m.dirty != nil {
+		return
+	}
+
+	read, _ := m.read.Load().(readOnly[K, V])
+	m.dirty = make(map[K]*entry[V], len(read.m))
+	for k, e := range read.m {
+		if !e.tryExpungeLocked() {
+			m.dirty[k] = e
+		}
+	}
+}
+
+// LoadAndDelete deletes the value for a key, returning the previous value if any.
+// The loaded result reports whether the key was present.
+func (m *Map[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
+	read, _ := m.read.Load().(readOnly[K, V])
+	e, ok := read.m[key]
+	if !ok && read.amended {
+		m.mu.Lock()
+		read, _ = m.read.Load().(readOnly[K, V])
+		e, ok = read.m[key]
+		if !ok && read.amended {
+			e, ok = m.dirty[key]
+			delete(m.dirty, key)
+			// Regardless of whether the entry was present, record a miss: this key
+			// will take the slow path until the dirty map is promoted to the read
+			// map.
+			m.missLocked()
+		}
+		m.mu.Unlock()
+	}
+	if ok {
+		return e.delete()
+	}
+	return value, false
+}
+
+// Delete deletes the value for a key.
+func (m *Map[K, V]) Delete(key K) {
+	m.LoadAndDelete(key)
 }
 
 // Range calls f sequentially for each key and value present in the map. If f returns false, range stops the iteration.
 func (m *Map[K, V]) Range(f func(key K, value V) bool) {
-	m.m.Range(func(key, value any) bool { return f(key.(K), value.(V)) })
-}
+	// We need to be able to iterate over all of the keys that were already
+	// present at the start of the call to Range.
+	// If read.amended is false, then read.m satisfies that property without
+	// requiring us to hold m.mu for a long time.
+	read, _ := m.read.Load().(readOnly[K, V])
+	if read.amended {
+		// m.dirty contains keys not in read.m. Fortunately, Range is already O(N)
+		// (assuming the caller does not break out early), so a call to Range
+		// amortizes an entire copy of the map: we can promote the dirty copy
+		// immediately!
+		m.mu.Lock()
+		read, _ = m.read.Load().(readOnly[K, V])
+		if read.amended {
+			read = readOnly[K, V]{m: m.dirty}
+			m.read.Store(read)
+			m.dirty = nil
+			m.misses = 0
+		}
+		m.mu.Unlock()
+	}
 
-// Store stores the given value for a key.
-func (m *Map[K, V]) Store(key K, value V) { m.m.Store(key, value) }
+	for k, e := range read.m {
+		v, ok := e.load()
+		if !ok {
+			continue
+		}
+		if !f(k, v) {
+			break
+		}
+	}
+}
---
+++ map_bench_test.go
@@ -0,0 +1,562 @@
+package gosync
+
+import (
+	"fmt"
+	"reflect"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+// RWMutexMap is an implementation of mapInterface using a sync.RWMutex.
+type RWMutexMap struct {
+	mu    sync.RWMutex
+	dirty map[interface{}]interface{}
+}
+
+func (m *RWMutexMap) Load(key interface{}) (value interface{}, ok bool) {
+	m.mu.RLock()
+	value, ok = m.dirty[key]
+	m.mu.RUnlock()
+	return
+}
+
+func (m *RWMutexMap) Store(key, value interface{}) {
+	m.mu.Lock()
+	if m.dirty == nil {
+		m.dirty = make(map[interface{}]interface{})
+	}
+	m.dirty[key] = value
+	m.mu.Unlock()
+}
+
+func (m *RWMutexMap) LoadOrStore(key, value interface{}) (actual interface{}, loaded bool) {
+	m.mu.Lock()
+	actual, loaded = m.dirty[key]
+	if !loaded {
+		actual = value
+		if m.dirty == nil {
+			m.dirty = make(map[interface{}]interface{})
+		}
+		m.dirty[key] = value
+	}
+	m.mu.Unlock()
+	return actual, loaded
+}
+
+func (m *RWMutexMap) LoadAndDelete(key interface{}) (value interface{}, loaded bool) {
+	m.mu.Lock()
+	value, loaded = m.dirty[key]
+	if loaded {
+		delete(m.dirty, key)
+	}
+	m.mu.Unlock()
+	return value, loaded
+}
+
+func (m *RWMutexMap) Delete(key interface{}) {
+	m.mu.Lock()
+	delete(m.dirty, key)
+	m.mu.Unlock()
+}
+
+func (m *RWMutexMap) Range(f func(key, value interface{}) (shouldContinue bool)) {
+	m.mu.RLock()
+	keys := make([]interface{}, 0, len(m.dirty))
+	for k := range m.dirty {
+		keys = append(keys, k)
+	}
+	m.mu.RUnlock()
+
+	for _, k := range keys {
+		v, ok := m.Load(k)
+		if !ok {
+			continue
+		}
+		if !f(k, v) {
+			break
+		}
+	}
+}
+
+// DeepCopyMap is an implementation of mapInterface using a Mutex and
+// atomic.Value.  It makes deep copies of the map on every write to avoid
+// acquiring the Mutex in Load.
+type DeepCopyMap struct {
+	mu    sync.Mutex
+	clean atomic.Value
+}
+
+func (m *DeepCopyMap) Load(key interface{}) (value interface{}, ok bool) {
+	clean, _ := m.clean.Load().(map[interface{}]interface{})
+	value, ok = clean[key]
+	return value, ok
+}
+
+func (m *DeepCopyMap) Store(key, value interface{}) {
+	m.mu.Lock()
+	dirty := m.dirty()
+	dirty[key] = value
+	m.clean.Store(dirty)
+	m.mu.Unlock()
+}
+
+func (m *DeepCopyMap) LoadOrStore(key, value interface{}) (actual interface{}, loaded bool) {
+	clean, _ := m.clean.Load().(map[interface{}]interface{})
+	actual, loaded = clean[key]
+	if loaded {
+		return actual, loaded
+	}
+
+	m.mu.Lock()
+	// Reload clean in case it changed while we were waiting on m.mu.
+	clean, _ = m.clean.Load().(map[interface{}]interface{})
+	actual, loaded = clean[key]
+	if !loaded {
+		dirty := m.dirty()
+		dirty[key] = value
+		actual = value
+		m.clean.Store(dirty)
+	}
+	m.mu.Unlock()
+	return actual, loaded
+}
+
+func (m *DeepCopyMap) LoadAndDelete(key interface{}) (value interface{}, loaded bool) {
+	m.mu.Lock()
+	dirty := m.dirty()
+	value, loaded = dirty[key]
+	if loaded {
+		delete(dirty, key)
+		m.clean.Store(dirty)
+	}
+	m.mu.Unlock()
+	return value, loaded
+}
+
+func (m *DeepCopyMap) Delete(key interface{}) {
+	m.mu.Lock()
+	dirty := m.dirty()
+	delete(dirty, key)
+	m.clean.Store(dirty)
+	m.mu.Unlock()
+}
+
+func (m *DeepCopyMap) Range(f func(key, value interface{}) (shouldContinue bool)) {
+	clean, _ := m.clean.Load().(map[interface{}]interface{})
+	for k, v := range clean {
+		if !f(k, v) {
+			break
+		}
+	}
+}
+
+func (m *DeepCopyMap) dirty() map[interface{}]interface{} {
+	clean, _ := m.clean.Load().(map[interface{}]interface{})
+	dirty := make(map[interface{}]interface{}, len(clean)+1)
+	for k, v := range clean {
+		dirty[k] = v
+	}
+	return dirty
+}
+
+type bench struct {
+	setup func(*testing.B, mapInterface)
+	perG  func(b *testing.B, pb *testing.PB, i int, m mapInterface)
+}
+
+func benchMap(b *testing.B, bench bench) {
+	for _, m := range [...]mapInterface{&DeepCopyMap{}, &RWMutexMap{}, &sync.Map{}} {
+		b.Run(fmt.Sprintf("%T", m), func(b *testing.B) {
+			m = reflect.New(reflect.TypeOf(m).Elem()).Interface().(mapInterface)
+			if bench.setup != nil {
+				bench.setup(b, m)
+			}
+
+			b.ResetTimer()
+
+			var i int64
+			b.RunParallel(func(pb *testing.PB) {
+				id := int(atomic.AddInt64(&i, 1) - 1)
+				bench.perG(b, pb, id*b.N, m)
+			})
+		})
+	}
+}
+
+func BenchmarkLoadMostlyHits(b *testing.B) {
+	const hits, misses = 1023, 1
+
+	benchMap(b, bench{
+		setup: func(_ *testing.B, m mapInterface) {
+			for i := 0; i < hits; i++ {
+				m.LoadOrStore(i, i)
+			}
+			// Prime the map to get it into a steady state.
+			for i := 0; i < hits*2; i++ {
+				m.Load(i % hits)
+			}
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				m.Load(i % (hits + misses))
+			}
+		},
+	})
+
+	b.Run("gosync.Map", func(b *testing.B) {
+		var m Map[int, int]
+		// setup:
+		for i := 0; i < hits; i++ {
+			m.LoadOrStore(i, i)
+		}
+		for i := 0; i < hits*2; i++ {
+			m.Load(i % hits)
+		}
+
+		b.ResetTimer()
+
+		perG := func(b *testing.B, pb *testing.PB, i int, m Map[int, int]) {
+			for ; pb.Next(); i++ {
+				m.Load(i % (hits + misses))
+			}
+		}
+		var i int64
+		b.RunParallel(func(pb *testing.PB) {
+			id := int(atomic.AddInt64(&i, 1) - 1)
+			perG(b, pb, id*b.N, m)
+		})
+	})
+}
+
+func BenchmarkLoadMostlyMisses(b *testing.B) {
+	const hits, misses = 1, 1023
+
+	benchMap(b, bench{
+		setup: func(_ *testing.B, m mapInterface) {
+			for i := 0; i < hits; i++ {
+				m.LoadOrStore(i, i)
+			}
+			// Prime the map to get it into a steady state.
+			for i := 0; i < hits*2; i++ {
+				m.Load(i % hits)
+			}
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				m.Load(i % (hits + misses))
+			}
+		},
+	})
+
+	b.Run("gosync.Map", func(b *testing.B) {
+		var m Map[int, int]
+		// setup:
+		for i := 0; i < hits; i++ {
+			m.LoadOrStore(i, i)
+		}
+		for i := 0; i < hits*2; i++ {
+			m.Load(i % hits)
+		}
+
+		// reset:
+		b.ResetTimer()
+
+		// perG:
+		perG := func(b *testing.B, pb *testing.PB, i int, m Map[int, int]) {
+			for ; pb.Next(); i++ {
+				m.Load(i % (hits + misses))
+			}
+		}
+		var i int64
+		b.RunParallel(func(pb *testing.PB) {
+			id := int(atomic.AddInt64(&i, 1) - 1)
+			perG(b, pb, id*b.N, m)
+		})
+	})
+}
+
+func BenchmarkLoadOrStoreBalanced(b *testing.B) {
+	const hits, misses = 128, 128
+
+	benchMap(b, bench{
+		setup: func(b *testing.B, m mapInterface) {
+			if _, ok := m.(*DeepCopyMap); ok {
+				b.Skip("DeepCopyMap has quadratic running time.")
+			}
+			for i := 0; i < hits; i++ {
+				m.LoadOrStore(i, i)
+			}
+			// Prime the map to get it into a steady state.
+			for i := 0; i < hits*2; i++ {
+				m.Load(i % hits)
+			}
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				j := i % (hits + misses)
+				if j < hits {
+					if _, ok := m.LoadOrStore(j, i); !ok {
+						b.Fatalf("unexpected miss for %v", j)
+					}
+				} else {
+					if v, loaded := m.LoadOrStore(i, i); loaded {
+						b.Fatalf("failed to store %v: existing value %v", i, v)
+					}
+				}
+			}
+		},
+	})
+
+	// syncmap code:
+	b.Run("gosync.Map", func(b *testing.B) {
+		var m Map[int, int]
+		// setup:
+		for i := 0; i < hits; i++ {
+			m.LoadOrStore(i, i)
+		}
+		for i := 0; i < hits*2; i++ {
+			m.Load(i % hits)
+		}
+
+		// reset:
+		b.ResetTimer()
+
+		// perG:
+		perG := func(b *testing.B, pb *testing.PB, i int, m Map[int, int]) {
+			for ; pb.Next(); i++ {
+				j := i % (hits + misses)
+				if j < hits {
+					if _, ok := m.LoadOrStore(j, i); !ok {
+						b.Fatalf("unexpected miss for %v", j)
+					}
+				} else {
+					if v, loaded := m.LoadOrStore(i, i); loaded {
+						b.Fatalf("failed to store %v: existing value %v", i, v)
+					}
+				}
+			}
+		}
+
+		var i int64
+		b.RunParallel(func(pb *testing.PB) {
+			id := int(atomic.AddInt64(&i, 1) - 1)
+			perG(b, pb, id*b.N, m)
+		})
+	})
+}
+
+func BenchmarkLoadOrStoreUnique(b *testing.B) {
+	benchMap(b, bench{
+		setup: func(b *testing.B, m mapInterface) {
+			if _, ok := m.(*DeepCopyMap); ok {
+				b.Skip("DeepCopyMap has quadratic running time.")
+			}
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				m.LoadOrStore(i, i)
+			}
+		},
+	})
+
+	// syncmap code:
+	b.Run("gosync.Map", func(b *testing.B) {
+		var m Map[int, int]
+		// setup:
+
+		// reset:
+		b.ResetTimer()
+
+		// perG:
+		perG := func(b *testing.B, pb *testing.PB, i int, m Map[int, int]) {
+			for ; pb.Next(); i++ {
+				m.LoadOrStore(i, i)
+			}
+		}
+		var i int64
+		b.RunParallel(func(pb *testing.PB) {
+			id := int(atomic.AddInt64(&i, 1) - 1)
+			perG(b, pb, id*b.N, m)
+		})
+	})
+}
+
+func BenchmarkLoadOrStoreCollision(b *testing.B) {
+	benchMap(b, bench{
+		setup: func(_ *testing.B, m mapInterface) {
+			m.LoadOrStore(0, 0)
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				m.LoadOrStore(0, 0)
+			}
+		},
+	})
+
+	// syncmap code:
+	b.Run("gosync.Map", func(b *testing.B) {
+		var m Map[int, int]
+		b.ResetTimer()
+
+		// perG:
+		perG := func(b *testing.B, pb *testing.PB, i int, m Map[int, int]) {
+			for ; pb.Next(); i++ {
+				m.LoadOrStore(0, 0)
+			}
+		}
+		var i int64
+		b.RunParallel(func(pb *testing.PB) {
+			id := int(atomic.AddInt64(&i, 1) - 1)
+			perG(b, pb, id*b.N, m)
+		})
+	})
+}
+
+func BenchmarkRange(b *testing.B) {
+	const mapSize = 1 << 10
+
+	benchMap(b, bench{
+		setup: func(_ *testing.B, m mapInterface) {
+			for i := 0; i < mapSize; i++ {
+				m.Store(i, i)
+			}
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				m.Range(func(_, _ interface{}) bool { return true })
+			}
+		},
+	})
+
+	// syncmap code:
+	b.Run("gosync.Map", func(b *testing.B) {
+		var m Map[int, int]
+		// setup:
+
+		// reset:
+		b.ResetTimer()
+
+		// perG:
+		perG := func(b *testing.B, pb *testing.PB, i int, m Map[int, int]) {
+			for ; pb.Next(); i++ {
+				m.Range(func(_, _ int) bool { return true })
+			}
+		}
+		var i int64
+		b.RunParallel(func(pb *testing.PB) {
+			id := int(atomic.AddInt64(&i, 1) - 1)
+			perG(b, pb, id*b.N, m)
+		})
+	})
+}
+
+// BenchmarkAdversarialAlloc tests performance when we store a new value
+// immediately whenever the map is promoted to clean and otherwise load a
+// unique, missing key.
+//
+// This forces the Load calls to always acquire the map's mutex.
+func BenchmarkAdversarialAlloc(b *testing.B) {
+	benchMap(b, bench{
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			var stores, loadsSinceStore int64
+			for ; pb.Next(); i++ {
+				m.Load(i)
+				if loadsSinceStore++; loadsSinceStore > stores {
+					m.LoadOrStore(i, stores)
+					loadsSinceStore = 0
+					stores++
+				}
+			}
+		},
+	})
+
+	// syncmap code:
+	b.Run("gosync.Map", func(b *testing.B) {
+		var m Map[int, int]
+		b.ResetTimer()
+
+		perG := func(b *testing.B, pb *testing.PB, i int, m Map[int, int]) {
+			var stores, loadsSinceStore int
+			for ; pb.Next(); i++ {
+				m.Load(i)
+				if loadsSinceStore++; loadsSinceStore > stores {
+					m.LoadOrStore(i, stores)
+					loadsSinceStore = 0
+					stores++
+				}
+			}
+		}
+		var i int64
+		b.RunParallel(func(pb *testing.PB) {
+			id := int(atomic.AddInt64(&i, 1) - 1)
+			perG(b, pb, id*b.N, m)
+		})
+	})
+}
+
+// BenchmarkAdversarialDelete tests performance when we periodically delete
+// one key and add a different one in a large map.
+//
+// This forces the Load calls to always acquire the map's mutex and periodically
+// makes a full copy of the map despite changing only one entry.
+func BenchmarkAdversarialDelete(b *testing.B) {
+	const mapSize = 1 << 10
+
+	benchMap(b, bench{
+		setup: func(_ *testing.B, m mapInterface) {
+			for i := 0; i < mapSize; i++ {
+				m.Store(i, i)
+			}
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				m.Load(i)
+
+				if i%mapSize == 0 {
+					m.Range(func(k, _ interface{}) bool {
+						m.Delete(k)
+						return false
+					})
+					m.Store(i, i)
+				}
+			}
+		},
+	})
+
+	// syncmap code:
+	b.Run("gosync.Map", func(b *testing.B) {
+		var m Map[int, int]
+		// setup:
+
+		// reset:
+		b.ResetTimer()
+
+		// perG:
+		perG := func(b *testing.B, pb *testing.PB, i int, m Map[int, int]) {
+			for ; pb.Next(); i++ {
+				m.Load(i)
+
+				if i%mapSize == 0 {
+					m.Range(func(k, _ int) bool {
+						m.Delete(k)
+						return false
+					})
+					m.Store(i, i)
+				}
+			}
+		}
+		var i int64
+		b.RunParallel(func(pb *testing.PB) {
+			id := int(atomic.AddInt64(&i, 1) - 1)
+			perG(b, pb, id*b.N, m)
+		})
+	})
+}
